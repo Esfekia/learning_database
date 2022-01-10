7fbab5f1bdbfbd6b2908a947bd2694002b6185cd
esfekia <sertac@gmail.com>
2022-01-10 06:48:38 -0500

    Rolling Dice: initiated die.py and die_visual.py

---
+++ rolling_dice/die_visual.py
@@ -0,0 +1,17 @@
+from die import Die
+#Create a D6.
+die = Die()
+
+#Make somre rolls and store results in a list.
+results =[]
+for roll_num in range(1000):
+	result = die.roll()
+	results.append(result)
+
+#Analyze the results.
+frequencies = []
+for value in range(1,die.num_sides+1):
+	frequency= results.count(value)
+	frequencies.append(frequency)
+
+print (frequencies)
