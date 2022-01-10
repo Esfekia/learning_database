7c84920d6319b95d9eef08c943960f67ae83b8d1
esfekia <sertac@gmail.com>
2022-01-10 05:25:21 -0500

    Increased plot point numbers to 50,000, recreased size to 1.

--- randomwalk/rw_visual.py
+++ randomwalk/rw_visual.py
@@ -6,7 +6,7 @@ from random_walk import RandomWalk
 while True:
 
 	#Make a random walk.
-	rw =RandomWalk()
+	rw =RandomWalk(500_000)
 	rw.fill_walk()
 
 	#Plot the points in the walk.
@@ -14,11 +14,11 @@ while True:
 	fig, ax = plt.subplots()
 	point_numbers = range(rw.num_points)
 	ax.scatter(rw.x_values, rw.y_values, c= point_numbers, cmap =plt.cm.Blues, 
-		edgecolors ='none', s=15)
+		edgecolors ='none', s=1)
 
 	#Mark the startubg and ending points.
-	ax.scatter(0,0, c ='green', edgecolors ='none', s=100)
-	ax.scatter(rw.x_values[-1], rw.y_values[-1], c = 'red',edgecolors ='none', s=100)
+	ax.scatter(0,0, c ='green', edgecolors ='none', s=1)
+	ax.scatter(rw.x_values[-1], rw.y_values[-1], c = 'red',edgecolors ='none', s=1)
 	
 	#Remove the axes.
 	ax.get_xaxis().set_visible(False)
