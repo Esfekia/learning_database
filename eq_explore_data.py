ce122783a907e0d878b262c3f6ad7c724b844bbe
esfekia <sertac@gmail.com>
2022-01-12 06:56:12 -0500

    Extracted the lat and lon values and stored them in lists.

--- eq_explore_data.py
+++ eq_explore_data.py
@@ -7,12 +7,19 @@ with open (filename) as f:
 
 all_eq_dicts=all_eq_data['features']
 
-mags =[]
+mags, lons,lats =[],[],[]
 for eq_dicts in all_eq_dicts:
 	mag = eq_dicts['properties']['mag']
+	lon = eq_dicts['geometry']['coordinates'][0]
+	lat = eq_dicts['geometry']['coordinates'][1]
+
 	mags.append(mag)
+	lons.append(lon)
+	lats.append(lat)
 
 print (mags[:10])
+print (lons[:5])
+print (lats[:5])
 #print (len(all_eq_dicts))
 
 #readable_file = 'data/readable_eq_data.json'
